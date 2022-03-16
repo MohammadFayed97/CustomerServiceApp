a553730ee63b49c2cab5eb9a2a5e19059dad604c
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-03-16 19:19:34 +0200

    Create City Details Component

---
+++ CustomerServiceApp/Domains/Cities/Cities.Client/Components/CityDetailsComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace Cities.Client.Components
+{
+    using Cities.Shared.ViewModels;
+    using Microsoft.AspNetCore.Components;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public partial class CityDetailsComponent
+    {
+        [Parameter]
+        public Guid CityId { get; set; }
+
+        private CityViewModel cityViewModel = new CityViewModel();
+
+        protected override async Task OnInitializedAsync()
+        {
+            cityViewModel = await _cityHttpService.GetCityByIdAsync($"/api/cities/{CityId}");
+        }
+    }
+}
